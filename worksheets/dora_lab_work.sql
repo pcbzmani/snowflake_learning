c10a361aae3dbe83772bc9ebac4baa9c2ca319bf
pcbzmani <pcbzmani@gmail.com>
2023-03-27 22:49:31 +0530

    Create dora_lab_work.sql

---
+++ worksheets/dora_lab_work.sql
@@ -0,0 +1,199 @@
+--You may set these manually or you may edit the code on the next 3 lines
+use database DEMO_DB;
+use schema PUBLIC;
+use role ACCOUNTADMIN;
+
+--Do NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT
+ 'DWW01' as step
+ ,( select count(*)  
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA 
+   where schema_name in ('FLOWERS','VEGGIES','FRUITS')) as actual
+  ,3 as expected
+  ,'Created 3 Garden Plant schemas' as description
+); 
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW02' as step 
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA 
+   where schema_name = 'PUBLIC') as actual 
+ , 0 as expected 
+ ,'Deleted PUBLIC schema.' as description
+); 
+
+--Do NOT EDIT ANYTHING BELOW THIS LINE
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW03' as step 
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+   where table_name = 'ROOT_DEPTH') as actual 
+ , 1 as expected 
+ ,'ROOT_DEPTH Table Exists' as description
+); 
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE. THE CODE MUST BE RUN EXACTLY AS IT IS WRITTEN
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW04' as step
+ ,( select count(*) as SCHEMAS_FOUND 
+   from UTIL_DB.INFORMATION_SCHEMA.SCHEMATA) as actual
+ , 2 as expected
+ , 'UTIL_DB Schemas' as description
+); 
+
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE. THE CODE MUST BE RUN EXACTLY AS IT IS WRITTEN
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW05' as step
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+   where table_name = 'VEGETABLE_DETAILS') as actual
+ , 1 as expected
+ ,'VEGETABLE_DETAILS Table' as description
+); 
+
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE. THE CODE MUST BE RUN EXACTLY AS IT IS WRITTEN
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from ( 
+ SELECT 'DWW06' as step 
+,( select row_count 
+  from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+  where table_name = 'ROOT_DEPTH') as actual 
+, 3 as expected 
+,'ROOT_DEPTH row count' as description
+);  
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE. THE CODE MUST BE RUN EXACTLY AS IT IS WRITTEN
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW07' as step
+ ,( select row_count 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+   where table_name = 'VEGETABLE_DETAILS') as actual
+ , 41 as expected
+ , 'VEG_DETAILS row count' as description
+); 
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE. THE CODE MUST BE RUN EXACTLY AS IT IS WRITTEN
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from ( 
+   SELECT 'DWW08' as step 
+   ,( select count(*) 
+     from GARDEN_PLANTS.INFORMATION_SCHEMA.FILE_FORMATS 
+     where FIELD_DELIMITER =',' 
+     and FIELD_OPTIONALLY_ENCLOSED_BY ='"') as actual 
+   , 1 as expected 
+   , 'File Format 1 Exists' as description 
+); 
+
+-- DO NOT EDIT ANYTHING BELOW THIS LINE. THE CODE MUST BE RUN EXACTLY AS IT IS WRITTEN
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW09' as step
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.FILE_FORMATS 
+   where FIELD_DELIMITER ='|' 
+   ) as actual
+ , 1 as expected
+ ,'File Format 2 Exists' as description
+); 
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW10' as step
+  ,( select count(*) 
+    from GARDEN_PLANTS.INFORMATION_SCHEMA.stages
+    where stage_url='s3://uni-lab-files' 
+    and stage_type='External Named') as actual
+  , 1 as expected
+  , 'External stage created' as description
+ ); 
+
+
+ select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+  SELECT 'DWW11' as step
+  ,( select row_count 
+    from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+    where table_name = 'VEGETABLE_DETAILS_SOIL_TYPE') as actual
+  , 42 as expected
+  , 'Veg Det Soil Type Count' as description
+ ); 
+
+ select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (  
+      SELECT 'DWW12' as step 
+      ,( select row_count 
+        from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+        where table_name = 'VEGETABLE_DETAILS_PLANT_HEIGHT') as actual 
+      , 41 as expected 
+      , 'Veg Detail Plant Height Count' as description   
+); 
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (  
+     SELECT 'DWW13' as step 
+     ,( select row_count 
+       from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+       where table_name = 'LU_SOIL_TYPE') as actual 
+     , 8 as expected 
+     ,'Soil Type Look Up Table' as description   
+);
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from ( 
+     SELECT 'DWW14' as step 
+     ,( select count(*) 
+       from GARDEN_PLANTS.INFORMATION_SCHEMA.FILE_FORMATS 
+       where FILE_FORMAT_NAME='L8_CHALLENGE_FF' 
+       and FIELD_DELIMITER = '\t') as actual 
+     , 1 as expected 
+     ,'Challenge File Format Created' as description  
+); 
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (  
+     SELECT 'DWW15' as step 
+     ,( select count(*) 
+      from LIBRARY_CARD_CATALOG.PUBLIC.Book_to_Author ba 
+      join LIBRARY_CARD_CATALOG.PUBLIC.author a 
+      on ba.author_uid = a.author_uid 
+      join LIBRARY_CARD_CATALOG.PUBLIC.book b 
+      on b.book_uid=ba.book_uid) as actual 
+     , 6 as expected 
+     , '3NF DB was Created.' as description  
+); 
+
+
+-- Set your worksheet drop lists. DO NOT EDIT THE DORA CODE.
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+  SELECT 'DWW16' as step
+  ,( select row_count 
+    from LIBRARY_CARD_CATALOG.INFORMATION_SCHEMA.TABLES 
+    where table_name = 'AUTHOR_INGEST_JSON') as actual
+  ,6 as expected
+  ,'Check number of rows' as description
+ ); 
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (   
+     SELECT 'DWW17' as step 
+      ,( select row_count 
+        from LIBRARY_CARD_CATALOG.INFORMATION_SCHEMA.TABLES 
+        where table_name = 'NESTED_INGEST_JSON') as actual 
+      , 5 as expected 
+      ,'Check number of rows' as description  
+); 
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+   SELECT 'DWW18' as step
+  ,( select row_count 
+    from SOCIAL_MEDIA_FLOODGATES.INFORMATION_SCHEMA.TABLES 
+    where table_name = 'TWEET_INGEST') as actual
+  , 9 as expected
+  ,'Check number of rows' as description  
+ ); 
+
+ select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+  SELECT 'DWW19' as step
+  ,( select count(*) 
+    from SOCIAL_MEDIA_FLOODGATES.INFORMATION_SCHEMA.VIEWS 
+    where table_name = 'HASHTAGS_NORMALIZED') as actual
+  , 1 as expected
+  ,'Check number of rows' as description
+ ); 
