389ad911349084a2e619d16bff248e9e417e12ed
pcbzmani <pcbzmani@gmail.com>
2023-03-27 22:49:48 +0530

    Create stage.sql

---
+++ worksheets/stage.sql
@@ -0,0 +1,9 @@
+ create stage garden_plants.veggies.like_a_window_into_an_s3_bucket 
+ url = 's3://uni-lab-files';
+
+ list @like_a_window_into_an_s3_bucket;
+
+list @like_a_window_into_an_s3_bucket/this_;
+list @like_a_window_into_an_s3_bucket/THIS_;
+
+
